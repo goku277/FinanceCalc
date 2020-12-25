bcba39174463f9d555f37354d6c52bb0d7437067
Biswadeep Chakraborty <68191134+goku277@users.noreply.github.com>
2020-12-25 13:17:10 +0530

    Update MainActivity.java

--- app/src/main/java/com/team_three/MainActivity.java
+++ app/src/main/java/com/team_three/MainActivity.java
@@ -2,14 +2,35 @@ package com.team_three;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView splash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        splash= (ImageView) findViewById(R.id.splash_id);
+
+     //   Animation rotate= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate);
+     //   splash.setAnimation(rotate);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, UserDashboard.class));
+              //  finish();
+                finishAffinity();
+            }
+        }, 2000);
 
 
     }
